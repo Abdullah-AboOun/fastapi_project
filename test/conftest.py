63c0fdc7ccfb813d4028b85a786db8f737b79efc
Abdullah-Abo-Oun <80777415+Abdullah-Abu-Aoun@users.noreply.github.com>
2023-01-31 00:34:26 +0200

    test 3.0

--- test/conftest.py
+++ test/conftest.py
@@ -7,6 +7,9 @@ from app.config import settings
 from app.database import get_db, Base
 from app.oauth2 import create_access_token
 from app import models
+from alembic import command
+
+
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
 
@@ -16,7 +19,7 @@ TestingSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
@@ -27,7 +30,7 @@ def session():
         db.close()
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def client(session):
     def override_get_db():
 
@@ -39,13 +42,16 @@ def client(session):
     yield TestClient(app)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def test_user(client):
-    user_data = {"email": "test@gmail.com", "password": "pass1234"}
+    user_data = {"email": "test@gmail.com",
+                 "password": "pass1234"}
     res = client.post("/users/", json=user_data)
+
     assert res.status_code == 201
+
     new_user = res.json()
-    new_user["password"] = user_data["password"]
+    new_user['password'] = user_data['password']
     return new_user
 
 
@@ -105,6 +111,7 @@ def test_posts(test_user, session, test_user2):
     posts = list(post_map)
 
     session.add_all(posts)
+    session.commit()
 
     posts = session.query(models.Post).all()
     return posts
